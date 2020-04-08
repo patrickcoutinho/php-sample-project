933bab2407806598545f1f59afb073409c23a16a
Patrick Coutinho <patrick@istyle.com.br>
2020-04-07 21:50:02 -0300

    Changed to composer

--- core/bootstrap.php
+++ core/bootstrap.php
@@ -4,9 +4,4 @@ require 'core/envs.php';
 
 $config = require 'config.php';
 
-require 'core/Router.php';
-require 'core/Request.php';
-require 'core/db/Connection.php';
-require 'core/db/QueryBuilder.php';
-
 $db = new QueryBuilder(Connection::make($config['database']));
